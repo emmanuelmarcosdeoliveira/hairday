2de67155132e8e93ce754eb168093b1501fc7365
Emmanuel Marcos de Oliveira <emmmanuelmarcosdeoliveira@gmail.com>
2025-03-17 18:23:15 -0300

    chore:  added packages and dependencies to run webpack and babel

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Importação dos Estilos CSS
+import "./styles/form.css";
+import "./styles/global.css";
+import "./styles/schedule.css";
+
+// Importando o Day.js
+import "./libs/dayjs.js";
+
+import dayjs from "dayjs";
+console.log(dayjs().format("DD/MM - HH:MM"));
